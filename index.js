e58829ddb0db33e021650d328896be9cf93eccf9
Jesus <jryanez@jryanez.com>
2022-02-07 14:02:27 -0500

    Updated to include best or worst commands

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
-bot.on('ready', () => {
+bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-const jsonData = require('./champs');
-const values = Object.values(jsonData)
+const jsonData = require("./champs");
+const values = Object.values(jsonData);
 
-bot.on('message', msg => {
-  if (msg.content.startsWith('!lol')) {
-    const line = values[parseInt(Math.random() * values.length)]
+bot.on("message", (msg) => {
+  if (msg.content.startsWith("!best")) {
+    const line = values[parseInt(Math.random() * values.length)];
     msg.channel.send(`Who is the best ${line} in this server?`);
-  } 
+  } else if (msg.content.startsWith("!worst")) {
+    const line = values[parseInt(Math.random() * values.length)];
+    msg.channel.send(`Who is the worst ${line} in this server?`);
+  }
 });
